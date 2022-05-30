5ac91a238fab507f72d1588a5276b21c1ef2763d
MosNes <7hekarl@gmail.com>
2022-05-30 11:17:20 -0400

    initial JS code and testing of creating timeblock elements

---
+++ assets/scripts/script.js
@@ -0,0 +1,20 @@
+//Time Block Reference HTML
+/* <div class="time-block w-100 row d-flex flex-row">
+        <div class="hour col-1 p-3">Test hour</div>
+        <div class="description col-10 p-3">Test 1</div>
+        <div class="save-container col-1">
+          <button class="btn saveBtn w-100 h-100"><i class="fa-solid fa-floppy-disk"></i></button>
+        </div> */
+
+// ------GLOBAL VARIABLES------
+var todaysDateEl = $('#currentDay');
+
+var todaysDate = "";
+
+var timeBlockEl = $("<div>").addClass("time-block w-100 row d-flex flex-row").html('<div class="hour col-1 p-3">Test hour</div><div class="description col-10 p-3">Test 1</div><div class="save-container col-1"><button class="btn saveBtn w-100 h-100"><i class="fa-solid fa-floppy-disk"></i></button>');
+
+console.log(timeBlockEl);
+
+// ------FUNCTIONS------
+
+// ------INITIALIZATIONS------
