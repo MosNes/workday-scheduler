6236b902ec87e80d753207bdd5319c7dc13d5210
MosNes <7hekarl@gmail.com>
2022-05-30 12:41:26 -0400

    switched from date-fns to luxon, tested creation of current date

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,9 +7,12 @@
         </div> */
 
 // ------GLOBAL VARIABLES------
+const DateTime = luxon.DateTime;
 var todaysDateEl = $('#currentDay');
 
-var todaysDate = "";
+var timeBlockContainerEl = $('#timeblock-container');
+
+var todaysDate = DateTime.now();
 
 var timeBlockEl = $("<div>").addClass("time-block w-100 row d-flex flex-row").html('<div class="hour col-1 p-3">Test hour</div><div class="description col-10 p-3">Test 1</div><div class="save-container col-1"><button class="btn saveBtn w-100 h-100"><i class="fa-solid fa-floppy-disk"></i></button>');
 
